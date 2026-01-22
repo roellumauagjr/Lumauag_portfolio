43409dc95efbf46545f935a1794d1e344d7896fb
ROEL JR. SALVADOR LUMAUAG <2100195@cc.usls.edu>
2026-01-22 14:06:36 +0800

    Lab Activity 1: Routes, Middleware and Blade Layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
+@extends('layouts.content')
+
+@section('title', 'Home')
+
+@section('content')
+
 <h1>Welcome to my Portfolio</h1>
+<p> This is for all the ladies, who don't get hype enough. Here then gone chameleon.</p>
+@endsection
